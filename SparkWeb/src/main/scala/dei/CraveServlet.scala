7efa252140354ec59107932c5090b420678058e0
Roy Cohen <roee78@users.noreply.github.com>
2016-07-22 21:28:28 -0700

    better recommendations by using weighted scores based on number of reviews

--- SparkWeb/src/main/scala/dei/CraveServlet.scala
+++ SparkWeb/src/main/scala/dei/CraveServlet.scala
@@ -1,5 +1,8 @@
 package dei
 
+import java.util
+
+import com.datastax.driver.core.Row
 import org.apache.spark
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.tree.model.RandomForestModel
@@ -11,7 +14,9 @@ import com.datastax.spark.connector._
 import org.apache.spark.sql.{DataFrame, DataFrameHolder, Row}
 import org.apache.spark.sql.types._
 import com.datastax.spark.connector.cql.CassandraConnector
+
 import scala.collection.JavaConversions._
+import scala.collection.mutable
 
 
 
@@ -103,9 +108,9 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
   def roundAt(p: Int)(n: Double): Double = { val s = math pow (10, p); (math round n * s) / s }
 
 
-  def getRecommendations(top: scala.collection.mutable.Buffer[com.datastax.driver.core.Row], businessMap: scala.collection.mutable.Map[String,BusinessObj]) = {
+  def getRecommendationObjects(top: mutable.Buffer[(Double, com.datastax.driver.core.Row)], businessMap: collection.mutable.Map[String,BusinessObj]) = {
 
-    val recommendations = top.map(row => {
+    val recommendations = top.map{case(score,row) =>
       val dish = row.getString("dish")
       val id = row.getString("businessid")
       val tmp = row.getDouble("avgrating")
@@ -118,7 +123,7 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
       val stars = businessMap(id).stars
 
       new DataPoint(dish, id, avgrating, dishnumreviews, promotext, address, name, restaurantnumreviews, stars)
-    }).toList
+    }.toList
 
     recommendations
   }
@@ -128,11 +133,9 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
       val session = connector.openSession
 
       val reviews = session.execute("SELECT dish, businessid, avgrating, numreviews, promotext FROM dishes_db.dishes WHERE citystate = ? AND dish IN (" + dishes +")", citystate)
-      val rows = reviews.all
+      val top = getRecommendations(reviews.all).take(4)
 
-      val top = rows.sortBy(row => -row.getDouble("avgrating")).take(4)
-
-      val businessids = top.map(row => row.getString("businessid")).mkString("'", "','", "'")
+      val businessids = top.map{case(score,row) => row.getString("businessid")}.mkString("'", "','", "'")
       val businesses = session.execute("SELECT businessid, full_address, name, reviewcount, stars FROM dishes_db.businesses WHERE citystate = ? AND businessid IN (" + businessids + ")", citystate)
       val bRows = businesses.all
 
@@ -140,7 +143,7 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
       session.close
 
       val businessMap = getBusinessMap(bRows)
-      val recommendations = getRecommendations(top, businessMap)
+      val recommendations = getRecommendationObjects(top, businessMap)
       recommendations
   }
 
@@ -170,11 +173,9 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
     val session = connector.openSession
 
     val reviews = session.execute("SELECT dish, businessid, avgrating, numreviews, promotext FROM dishes_db.dishes WHERE citystate = ?", citystate)
-    val rows = reviews.all
+    val top = getRecommendations(reviews.all).take(3)
 
-    val top = rows.sortBy(row => -row.getDouble("avgrating")).take(3)
-
-    val businessids = top.map(row => row.getString("businessid")).mkString("'","','","'")
+    val businessids = top.map{case(score,row) => row.getString("businessid")}.mkString("'","','","'")
     val businesses = session.execute("SELECT businessid, full_address, name, reviewcount, stars FROM dishes_db.businesses WHERE citystate = ? AND businessid IN (" + businessids + ")", citystate)
     val bRows = businesses.all
 
@@ -182,7 +183,7 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
     session.close
 
     val businessMap = getBusinessMap(bRows)
-    val recommendations = getRecommendations(top, businessMap)
+    val recommendations = getRecommendationObjects(top, businessMap)
     recommendations
   }
 
@@ -191,9 +192,7 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
     val session = connector.openSession
 
     val reviews = session.execute("SELECT dish, businessid, avgrating, numreviews, promotext FROM dishes_db.dishes WHERE citystate = ? AND businessid = ?", citystate, businessid)
-    val rows = reviews.all
-
-    val top = rows.sortBy(row => -row.getDouble("avgrating")).take(3)
+    val top = getRecommendations(reviews.all).take(3)
 
     val businesses = session.execute("SELECT businessid, full_address, name, reviewcount, stars FROM dishes_db.businesses WHERE citystate = ? AND businessid = ?", citystate, businessid)
     val bRows = businesses.all
@@ -202,7 +201,7 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
     session.close
 
     val businessMap = getBusinessMap(bRows)
-    val recommendations = getRecommendations(top, businessMap)
+    val recommendations = getRecommendationObjects(top, businessMap)
     recommendations
   }
 
@@ -230,5 +229,36 @@ class CraveServlet extends ScalatraServlet with JacksonJsonSupport {
       })
     }
     categoryItems
+  }
+
+  def getRecommendations(rows: util.List[com.datastax.driver.core.Row]) : mutable.Buffer[(Double, com.datastax.driver.core.Row)] = {
+
+    val filteredScores = rows.filter(row => row.getDouble("avgrating") >= 3.5)
+
+    val weightedScores = filteredScores.map(row => {
+
+      val numreviews = row.getInt("numreviews")
+      val avgrating = row.getDouble("avgrating")
+
+      val weight = numreviews match {
+        case x if (x >= 0 && x < 10) => 0.55;
+        case x if (x >= 10 && x < 25) => 0.60;
+        case x if (x >= 25 && x < 50) => 0.65;
+        case x if (x >= 50 && x < 75) => 0.70;
+        case x if (x >= 75 && x < 100) => 0.75;
+        case x if (x >= 100 && x < 125) => 0.80;
+        case x if (x >= 125 && x < 150) => 0.85;
+        case x if (x >= 150 && x < 175) => 0.90;
+        case x if (x >= 175 && x < 500) => 0.95;
+        case x if (x >= 500) => 1.00;
+      }
+
+      val weightedScore = avgrating * weight
+
+      (weightedScore, row)
+    })
+
+    val recommendations = weightedScores.sortBy{case (score,row) => -score}
+    recommendations
   }
 }
