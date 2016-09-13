2780619757505f734bb8fd31ba15cee1deaa10d9
Roy Cohen <roee78@users.noreply.github.com>
2016-09-13 15:36:34 -0700

    .

--- GrubHubScraper/src/main/java/rc/App.java
+++ GrubHubScraper/src/main/java/rc/App.java
@@ -1,10 +1,8 @@
 package rc;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -18,26 +16,17 @@ public class App
 {
     public static void main( String[] args ) throws InterruptedException {
 
-        //TreeSet<String> dishes = new TreeSet<String>();
-
-
         List<String> prefixes = getPrefixes();
         int index = 0;
-//        for (String s : prefixes){
-//            System.out.println(++index + ":" + s);
-//        }
 
         HashMap<String, TreeSet<String>> typesAndDishes = new HashMap<String, TreeSet<String>>();
         for (String prefix : prefixes){
 
-            //if (index < 10){
                 String grubHubUrl = "https://api-gtm.grubhub.com/autocomplete?lat=37.75714874&lng=-122.42819214&locationMode=delivery&prefix=" + prefix + "&resultCount=100&resultTypeList=dishTerm";
                 String jsonResponse = doRequest(grubHubUrl);
                 System.out.println(prefix + ":   " + jsonResponse);
                 jsonParse(jsonResponse, typesAndDishes);
-//                Thread.sleep(500);
                 index++;
-            //}
         }
         //printDishes(typesAndDishes);
         saveDishes(typesAndDishes);
@@ -64,23 +53,6 @@ public class App
                 prefix = prefix.substring(0, prefix.length() - 1);
             }
         }
-
-//        for (int i = 0; i < alphabet.length(); i++){
-//
-//            String prefix = String.valueOf(alphabet.charAt(i));
-//
-//            for (int j = 0; j < alphabet.length(); j++) {
-//
-//                prefix += String.valueOf(alphabet.charAt(j));
-//
-//                for (int k = 0; k < alphabet.length(); k++) {
-//                    prefix += String.valueOf(alphabet.charAt(k));
-//                    prefixes.add(prefix);
-//                    prefix = prefix.substring(0, prefix.length() - 1);
-//                }
-//                prefix = prefix.substring(0, prefix.length() - 1);
-//            }
-//        }
 
         return prefixes;
     }
@@ -128,8 +100,6 @@ public class App
     private static String doRequest(String urlStr){
 
         DefaultHttpClient httpclient = new DefaultHttpClient();
-        //String urlStr = "https://api-gtm.grubhub.com/autocomplete?lat=37.75714874&lng=-122.42819214&locationMode=delivery&prefix=" + prefix + "&resultCount=100&resultTypeList=dishTerm";
-        //String urlStr = "https://api-gtm.grubhub.com/autocomplete?lat=37.75714874&lng=-122.42819214&locationMode=delivery&prefix=ha&resultCount=8&resultTypeList=dishTerm";
 
         HttpGet httpget = new HttpGet(urlStr);
         httpget.addHeader("Content-Type", "application/json");
@@ -154,10 +124,8 @@ public class App
         return json.toString();
     }
 
-    //private static void jsonParse(String str, TreeSet<String> dishes){
-        //str = "{\"age\":100, \"name\":\"mkyong.com\", \"messages\":[\"msg 1\",\"msg 2\",\"msg 3\"]}";
     private static void jsonParse(String str, HashMap<String, TreeSet<String>> typesAndDishes){
-        //HashMap<String, TreeSet<String>> typesAndDishes = new HashMap<String, TreeSet<String>>();
+
         JSONParser parser = new JSONParser();
 
         try {
@@ -167,26 +135,21 @@ public class App
             JSONObject resultListObj = (JSONObject)resultListArr.get(0);
             String resultType = resultListObj.get("result_type").toString();
 
+            JSONArray compListArr = (JSONArray)resultListObj.get("completion_list");
 
+            for (Object itemObj : compListArr){
+                JSONObject item = (JSONObject)itemObj;
 
-            //if (type.equalsIgnoreCase("dishTerm")){
-                JSONArray compListArr = (JSONArray)resultListObj.get("completion_list");
-
-                for (Object itemObj : compListArr){
-                    JSONObject item = (JSONObject)itemObj;
-                    //if (item.get("type").toString().equalsIgnoreCase("dish")){
-                    String type = item.get("type").toString();
-                    if (!typesAndDishes.containsKey(type)){
-                        typesAndDishes.put(type, new TreeSet<String>());
-                    }
-
-                    TreeSet<String> dishes = typesAndDishes.get(type);
-                    dishes.add(item.get("value").toString());
-                    typesAndDishes.put(type, dishes);
-
-                    //}
+                String type = item.get("type").toString();
+                if (!typesAndDishes.containsKey(type)){
+                    typesAndDishes.put(type, new TreeSet<String>());
                 }
-            //}
+
+                TreeSet<String> dishes = typesAndDishes.get(type);
+                dishes.add(item.get("value").toString());
+                typesAndDishes.put(type, dishes);
+
+            }
 
         } catch (ParseException e) {
             e.printStackTrace();
